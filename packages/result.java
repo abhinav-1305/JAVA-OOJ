a4c05f7d13382b28e51494650267eb53b790086c
Abhinav Kumar <113254225+abhinav-1305@users.noreply.github.com>
2023-01-13 12:11:36 +0530

    Create result.java

---
+++ packages/result.java
@@ -0,0 +1,37 @@
+//package result;
+
+import CIE.Student;
+import CIE.Internals;
+import SEE.External;
+import java.util.*;
+class result
+{
+	public static void main(String args[])
+	{
+		double res;
+		Scanner scan = new Scanner(System.in);
+		int n,j;
+		System.out.println("Enter the no. of students : ");
+		n = scan.nextInt();
+		Internals ie[] = new Internals[n];
+		External ex[] = new External[n];
+		for(j=0;j<n;j++)
+		{
+			ie[j] = new Internals();
+			ex[j] = new External();
+			System.out.println("--> Student "+(j+1));
+			ex[j].getdata();
+			ex[j].setdata();
+			System.out.println("Enter the CIE and SEE marks: ");
+			ie[j].getdata();
+			for(int i=0;i<5;i++)
+			{
+				System.out.println("The Total marks is: "+ (ex[j].earr[i]+ie[j].arr[i]));
+			}
+			System.out.println("------------------------------------------------------------");
+		}
+	}
+}
+			
+
+
